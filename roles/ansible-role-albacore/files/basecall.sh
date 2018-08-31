f9b89233f9f18722675ac2ab84cc7ed840ead36a
Cloud User <centos@dowhyvm-2.openstacklocal>
2018-08-31 03:55:10 +0000

    fix path

--- roles/ansible-role-albacore/files/basecall.sh
+++ roles/ansible-role-albacore/files/basecall.sh
@@ -11,7 +11,7 @@ if [ $6 = '0' ]
 then
   singularity exec "$2"/TannerDowhy-albacore-playbook-master-latest.simg read_fast5_basecaller.py -i "$1" -r -t "$3" -s output -f "$4" -k "$5" -o fastq,fast5 --disable_filtering --disable_pings; 
 else
-  singularity exec "$2"/TannerDowhy-albacore-playbook-master-latest.simg read_fast5_basecaller.py -i "$1" -r -t "$3" -s output -f "$4" -k "$5" -o fastq,fast5 --disable_filtering --disable_pings --barcoding;
+  singularity exec "$2"/TannerDowhy-albacore-playbook-master-latest.simg read_fast5_basecaller.py -i "$1" -r -t "$3" -s scratch/basecalling/output -f "$4" -k "$5" -o fastq,fast5 --disable_filtering --disable_pings --barcoding;
 fi
 
 exit 0;
